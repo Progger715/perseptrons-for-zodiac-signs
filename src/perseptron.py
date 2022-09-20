4d54013c36e68696e6eff2a9cce82f45948ec0af
Progger715 <83240866+Progger715@users.noreply.github.com>
2022-09-20 19:29:39 +0300

    Detects from 2 to 6 signs. after 6 there are no pictures yet. 1 picture is not detected.

--- src/perseptron.py
+++ src/perseptron.py
@@ -22,7 +22,7 @@ zodiac_signs = ["Aries", "Taurus", "Gemini",
 image = ''
 weights = []
 w = 1
-bias = 70
+bias = 20
 
 
 def init_weights():
@@ -53,7 +53,7 @@ def identify_image():
     height = image.size[1]
     pix = image.load()
     white_pixel = 255 * 3
-    net = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    net = [0, 0, 0, 0, 0, 0]#, 0, 0, 0, 0, 0, 0]
     count = 0
 
     for x in range(width):
@@ -68,17 +68,17 @@ def identify_image():
                 # print(f"({x}, {y})\tr = {r}\tg = {g}\tb = {b}")
             else:
                 s = 0
-            for i in range(12):
+            for i in range(6):
                 net[i] += s * weights[i][x][y]
     print("net = ", net)
     print("count = ", count)
-    print(choose_name_image(net))
+    # print("choose_name_image = ", choose_name_image(net))
     return choose_name_image(net)
 
 
 def choose_name_image(net):
     res = []
-    for i in range(12):
+    for i in range(6):
         if net[i] >= bias:
             res.append([net[i], i])
     # print("res = ", res)
@@ -89,12 +89,21 @@ def choose_name_image(net):
             if res[i][0] > max:
                 max = res[i][0]
                 max_index = res[i][1]
+        print("choose_name_image = ", zodiac_signs[max_index])
         return zodiac_signs[max_index]
     else:
         if len(res) > 0:
+            print("choose_name_image = ", zodiac_signs[res[0][1]])
             return zodiac_signs[res[0][1]]
         else:
+            print("choose_name_image = None")
             return None
+
+
+def choose_name_image_bew(net):
+    for i in range(6):
+        if net[i] >= bias:
+            return zodiac_signs[i]
 
 
 def add_weight(number):
@@ -128,9 +137,9 @@ def train():
     for _, _, files in os.walk(path):
         pass
     print(files, "\n")
-    print(len(files))
+    print("len(files)", len(files))
 
-    for i in range(21):
+    for i in range(42):
         global image
         image = open_image(files[i])
         res = identify_image()
@@ -143,16 +152,55 @@ def train():
                 # print_weights(number)
             else:
                 print("\n[OUT]\t", files[i].lower(), res.lower())
+                number = zodiac_signs.index(res)
+                print(number)
+                reduce_weight(number)
+                # print_weights(number)
         except Exception as ex:
             print("[TRAIN] net < bias\t", ex)
+
+
+def train1():
+    path = Path(Path.cwd().parent, "picture")
+    for _, _, files in os.walk(path):
+        pass
+    print(files, "\n")
+    print("len(files) =", len(files))
+
+    for i in range(10000):
+        global image
+        number_file = random.randint(0, len(files)-1)
+        image = open_image(files[number_file])
+        res = identify_image()
+        print("Image = ", files[number_file])
+        if res is None:
+            print(f"[None]")
+            break
+        # try:
+        if files[number_file].lower().startswith(res.lower()):
+            print(f"[TRUE]")
+            number = zodiac_signs.index(res)
+            print("number true image = ", number)
+            add_weight(number)
+            print()
+            # print_weights(number)
+        else:
+            print("[FALSE]")
+            number = zodiac_signs.index(res)
+            print("number false image = ", number)
+            reduce_weight(number)
+            # print_weights(number)
+            print()
+        # except Exception as ex:
+        #     print("[TRAIN] net < bias\t", ex)
 
 
 init_weights()
 # print("weight before training:")
 # print_weights(1)
-# image = open_image("тест32.png")
+# image = open_image("gemini1.png")
 # identify_image()
-train()
+train1()
 
-# print("weight after training:")
-# print_weights(1)
+print("weight after training:")
+print_weights(0)
