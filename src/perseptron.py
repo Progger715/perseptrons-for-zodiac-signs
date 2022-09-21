7b611a9dbb637be35fc33c28a2d256803536703d
Progger715 <83240866+Progger715@users.noreply.github.com>
2022-09-21 17:27:25 +0300

    function train_evenly() work true. training is working.

--- src/perseptron.py
+++ src/perseptron.py
@@ -22,7 +22,7 @@ zodiac_signs = ["Aries", "Taurus", "Gemini",
 image = ''
 weights = []
 w = 1
-bias = 20
+bias = 40
 
 
 def init_weights():
@@ -34,9 +34,15 @@ def init_weights():
                 weights[z][i].append(w)
 
 
-def print_weights(number):
+def print_weight(number):
     for i in range(32):
         print(weights[number][i])
+
+
+def print_all_weights():
+    for i in range(12):
+        print("\nweight for", zodiac_signs[i])
+        print_weight(i)
 
 
 def open_image(name_image):
@@ -51,10 +57,10 @@ def open_image(name_image):
 def identify_image():
     width = image.size[0]
     height = image.size[1]
-    pix = image.load()
+    pix = image.load()  # массив пикселей
     white_pixel = 255 * 3
-    net = [0, 0, 0, 0, 0, 0]#, 0, 0, 0, 0, 0, 0]
-    count = 0
+    net = [0, 0, 0, 0, 0, 0]  # , 0, 0, 0, 0, 0, 0]
+    count = 0  # количество черных пикселей, отладочная информация
 
     for x in range(width):
         for y in range(height):
@@ -71,8 +77,7 @@ def identify_image():
             for i in range(6):
                 net[i] += s * weights[i][x][y]
     print("net = ", net)
-    print("count = ", count)
-    # print("choose_name_image = ", choose_name_image(net))
+    # print("count = ", count)
     return choose_name_image(net)
 
 
@@ -89,24 +94,18 @@ def choose_name_image(net):
             if res[i][0] > max:
                 max = res[i][0]
                 max_index = res[i][1]
-        print("choose_name_image = ", zodiac_signs[max_index])
+        # print("choose_name_image = ", zodiac_signs[max_index])
         return zodiac_signs[max_index]
     else:
         if len(res) > 0:
-            print("choose_name_image = ", zodiac_signs[res[0][1]])
+            # print("choose_name_image = ", zodiac_signs[res[0][1]])
             return zodiac_signs[res[0][1]]
         else:
-            print("choose_name_image = None")
+            # print("choose_name_image = None")
             return None
 
 
-def choose_name_image_bew(net):
-    for i in range(6):
-        if net[i] >= bias:
-            return zodiac_signs[i]
-
-
-def add_weight(number):
+def add_weight(number):  # номер знака зодиака
     pix = image.load()
     white_pixel = 255 * 3
     for x in range(image.size[0]):
@@ -132,75 +131,72 @@ def reduce_weight(number):
                 weights[number][x][y] -= 1
 
 
-def train():
+def train_random():
     path = Path(Path.cwd().parent, "picture")
     for _, _, files in os.walk(path):
         pass
-    print(files, "\n")
-    print("len(files)", len(files))
 
-    for i in range(42):
+    for i in range(7500):
+        print("#", i)
         global image
-        image = open_image(files[i])
-        res = identify_image()
-        try:
-            if files[i].lower().startswith(res.lower()):
-                print("\n[IN]\t", files[i].lower(), res.lower())
-                number = zodiac_signs.index(res)
-                print(number)
-                add_weight(number)
-                # print_weights(number)
-            else:
-                print("\n[OUT]\t", files[i].lower(), res.lower())
-                number = zodiac_signs.index(res)
-                print(number)
-                reduce_weight(number)
-                # print_weights(number)
-        except Exception as ex:
-            print("[TRAIN] net < bias\t", ex)
-
-
-def train1():
-    path = Path(Path.cwd().parent, "picture")
-    for _, _, files in os.walk(path):
-        pass
-    print(files, "\n")
-    print("len(files) =", len(files))
-
-    for i in range(10000):
-        global image
-        number_file = random.randint(0, len(files)-1)
-        image = open_image(files[number_file])
-        res = identify_image()
+        number_file = random.randint(0, len(files) - 1)
+        image = open_image(files[number_file])  # files[number_file] - имя файла
+        res = identify_image()  # имя знака зодиака, которое определила программа
         print("Image = ", files[number_file])
+        print("Result = ", res)
         if res is None:
-            print(f"[None]")
-            break
-        # try:
+            print(f"[None]\n")
+            continue
         if files[number_file].lower().startswith(res.lower()):
             print(f"[TRUE]")
             number = zodiac_signs.index(res)
             print("number true image = ", number)
             add_weight(number)
             print()
-            # print_weights(number)
         else:
             print("[FALSE]")
-            number = zodiac_signs.index(res)
-            print("number false image = ", number)
-            reduce_weight(number)
-            # print_weights(number)
+            number_false = zodiac_signs.index(res)
+            print("number false image = ", number_false)
+            reduce_weight(number_false)
+            add_weight()
             print()
-        # except Exception as ex:
-        #     print("[TRAIN] net < bias\t", ex)
 
 
-init_weights()
-# print("weight before training:")
-# print_weights(1)
-# image = open_image("gemini1.png")
-# identify_image()
-train1()
+def train_evenly():
+    file_record_false = open('False_answers_history.txt', 'w')
+    for number_era in range(50):  # era
+        for number_var in range(1, 21):
+            for number_sign in range(6):
+                print("#", number_era, number_sign, number_var)
+                global image
+                file_name = zodiac_signs[number_sign] + f"{number_var}" + ".png"
+                image = open_image(file_name)
+                res = identify_image()  # имя знака зодиака, которое определила программа
+                print("Image = ", file_name)
+                print("Result = ", res)
+                if res is None:
+                    print(f"[None]\n")
+                    continue
+                if file_name.lower().startswith(res.lower()):
+                    print(f"[TRUE]\n")
+                    number = zodiac_signs.index(res)
+                    add_weight(number)
+                else:
+                    print("[FALSE]\n")
+                    number_false = zodiac_signs.index(res)
+                    reduce_weight(number_false)
+                    add_weight(number_sign)
+                    index = f'# {number_era} {number_sign} {number_var}\nImage = {file_name}\nResult = {res}\n\n'
+                    file_record_false.write(index)
+    file_record_false.write("\n\n\n\nend train\n\n\n\n")
+    file_record_false.close()
 
-print("weight after training:")
-print_weights(0)
+
+if __name__ == '__main__':
+    init_weights()
+    print("weight before training:")
+    print_all_weights()
+    train_evenly()
+    # train_evenly()
+    print("weight after training:")
+    print_all_weights()
